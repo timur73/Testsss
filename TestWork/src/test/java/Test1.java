2b801ab7fad2686840da26a1c42e680512ce5c68
timsBt <tims.bekmat@gmail.com>
2023-03-31 19:05:55 +0400

    один

---
+++ TestWork/src/test/java/Test1.java
@@ -0,0 +1,18 @@
+import io.github.bonigarcia.wdm.WebDriverManager;
+import org.junit.jupiter.api.Test;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import java.util.concurrent.TimeUnit;
+
+public class Test1 {
+    @Test
+    public void test() {
+        System.out.println("Hi");
+    }
+
+    @Test
+    public void test2() {
+        System.out.println("Hihhh");
+    }
+}
